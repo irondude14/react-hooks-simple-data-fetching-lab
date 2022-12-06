c49d8d01fce57e63bf3519c86dd6e9d2d14d4a5a
Vladyslav <vkadyrov@protonmail.com>
2022-12-06 18:20:04 -0500

    commit tree f0ee0408ae132f853ce84448a854a47e2528b2b8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message);
+        setImage(data.message);
+      });
+  }, []);
+
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(image);
+
+  return <img src={image} alt="A Random Dog" />;
+}
+
+export default App;
